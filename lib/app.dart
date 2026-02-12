8bb1148782cbbe9034748619fcf8c159037fd3c4
Osman Ibrahiem <osman.ibrahiem187@gmail.com>
2026-02-12 23:01:34 +0200

    feat: Add localization

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 import 'core/routing/app_routes.dart';
+import 'generated/l10n.dart';
 
 class KutbiApp extends StatelessWidget {
   const KutbiApp({super.key});
@@ -12,6 +14,14 @@ class KutbiApp extends StatelessWidget {
       title: 'Kutbi',
       initialRoute: AppRoutes.initial,
       routes: AppRoutes.routes,
+      localizationsDelegates: [
+        S.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: S.delegate.supportedLocales,
+      locale: const Locale('ar'),
     );
   }
 }
