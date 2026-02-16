78a71a08fddf9e8cf688e58def837cd138bcfb27
Osman Ibrahiem <osman.ibrahiem187@gmail.com>
2026-02-16 09:50:25 +0200

    feat: Rename KutbiApp to App

--- lib/app.dart
+++ lib/app.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 
 import 'core/routing/app_routes.dart';
 
-class KutbiApp extends StatelessWidget {
-  const KutbiApp({super.key});
+class App extends StatelessWidget {
+  const App({super.key});
 
   @override
   Widget build(BuildContext context) {
