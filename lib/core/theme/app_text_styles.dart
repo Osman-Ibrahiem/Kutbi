bb0be93c4fb25de07c58b1d09872145f9afce8dd
Osman Ibrahiem <osman.ibrahiem187@gmail.com>
2026-02-17 10:48:07 +0200

    feat: Add widget themes

--- lib/core/theme/app_text_styles.dart
+++ lib/core/theme/app_text_styles.dart
@@ -6,9 +6,11 @@ class AppTextStyles {
   static TextTheme textTheme() {
     return const TextTheme(
       displayLarge: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+      headlineMedium: TextStyle(fontSize: 24, fontWeight: FontWeight.w600),
       titleLarge: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
-      bodyLarge: TextStyle(fontSize: 16),
-      bodyMedium: TextStyle(fontSize: 14),
+      bodyLarge: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+      bodyMedium: TextStyle(fontSize: 14, fontWeight: FontWeight.w400),
+      labelLarge: TextStyle(fontSize: 14, fontWeight: FontWeight.w600),
     );
   }
 }
