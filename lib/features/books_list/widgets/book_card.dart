867e54b128115e513f204e15f7e2e0f101eebf61
Osman Ibrahiem <osman.ibrahiem187@gmail.com>
2026-02-19 14:25:50 +0200

    feat: Add bookmark button to book card

--- lib/features/books_list/widgets/book_card.dart
+++ lib/features/books_list/widgets/book_card.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:kutbi/core/widgets/image_network.dart';
 import 'package:kutbi/domain/models/book.dart';
 
+import '../../bookmarks/widgets/bookmark_button.dart';
+
 class BookCard extends StatelessWidget {
   final Book book;
   final VoidCallback? onTap;
@@ -21,11 +23,22 @@ class BookCard extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             Expanded(
-              child: ImageNetwork(url: book.imageUrl, fit: BoxFit.cover),
+              child: Stack(
+                fit: StackFit.expand,
+                children: [
+                  ImageNetwork(url: book.imageUrl, fit: BoxFit.cover),
+                  PositionedDirectional(
+                    top: 2,
+                    end: 2,
+                    child: BookmarkButton(book: book),
+                  ),
+                ],
+              ),
             ),
             Padding(
               padding: const EdgeInsets.all(10),
               child: Column(
+                mainAxisSize: MainAxisSize.min,
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Text(
